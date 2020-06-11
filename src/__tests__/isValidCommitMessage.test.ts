6c264010679e846c7665bec270b7204151235259
Adrian Smijulj <adrian1358@gmail.com>
2020-06-11 12:51:50 +0200

    test: create a basic test

---
+++ src/__tests__/isValidCommitMessage.test.ts
@@ -0,0 +1,12 @@
+import isValidCommitMessage from "../isValidCommitMesage";
+
+test("should be able to correctly validate the commit message", () => {
+    expect(isValidCommitMessage("fix: menu must open on shortcut press")).toBe(true);
+    expect(isValidCommitMessage("something: should not work")).toBe(false);
+    expect(isValidCommitMessage("fixes something")).toBe(false);
+    expect(isValidCommitMessage("🚧 fix: menu must open on shortcut press")).toBe(true);
+    expect(isValidCommitMessage("fix(menus): menu must open on shortcut press")).toBe(true);
+    expect(isValidCommitMessage("🚧 fix(menus): menu must open on shortcut press")).toBe(true);
+    expect(isValidCommitMessage("🚧 fixing something")).toBe(false);
+    expect(isValidCommitMessage("🚧 something: should not work")).toBe(false);
+});
