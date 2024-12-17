3426484d170d335ed635c512ad5f5952d6eb1de3
Andreas Kjellqvist <andreas.kjellqvist@timeedit.com>
2024-12-17 15:24:17 +0100

    Improved type safety

--- src/extractCommits.ts
+++ src/extractCommits.ts
@@ -12,32 +12,59 @@ export const extractCommits = async (
     // For "push" events, commits can be found in the "context.payload.commits".
     const pushCommits = context.payload.commits;
     if (Array.isArray(pushCommits)) {
+        assertCommitArray(pushCommits);
         return pushCommits;
     }
+    const commits_url = context.payload.pull_request?.commits_url;
+    if (typeof commits_url !== "string") {
+        return [];
+    }
+    const githubToken = getInput("github-token");
+    return getPrCommits({ prCommitsUrl: commits_url, githubToken });
+};
 
+interface GetPrCommitsProps {
+    prCommitsUrl: string;
+    githubToken: string;
+}
+async function getPrCommits(
+    { prCommitsUrl, githubToken }: GetPrCommitsProps,
+): Promise<Commit[]> {
     // For PRs, we need to get a list of commits via the GH API:
-    const prCommitsUrl: string | undefined = context.payload.pull_request
-        ?.commits_url;
-    if (prCommitsUrl) {
-        try {
-            const githubToken = getInput("GITHUB_TOKEN");
-            const requestHeaders: Record<string, string> = {
-                "Accept": "application/vnd.github+json",
-                ...(githubToken === ""
-                    ? {}
-                    : { "Authorization": `token ${githubToken}` }),
-            };
+    const requestHeaders: Record<string, string> = {
+        "Accept": "application/vnd.github+json",
+        ...(githubToken === ""
+            ? {}
+            : { "Authorization": `token ${githubToken}` }),
+    };
 
-            const { body } = await got.get(prCommitsUrl, {
-                responseType: "json",
-                headers: requestHeaders,
-            });
-
-            return Array.isArray(body) ? body.map((item) => item.commit) : [];
-        } catch {
+    try {
+        const { body } = await got.get(prCommitsUrl, {
+            responseType: "json",
+            headers: requestHeaders,
+        });
+        if (!Array.isArray(body)) {
             return [];
         }
-    }
 
-    return [];
-};
+        const commits = body.map((item) => {
+            return item?.commit;
+        });
+        assertCommitArray(commits);
+        return commits;
+    } catch {
+        return [];
+    }
+}
+
+function assertCommitArray(commits: unknown[]): asserts commits is Commit[] {
+    commits.forEach(assertIsCommit);
+    function assertIsCommit(item: unknown): asserts item is Commit {
+        if (
+            typeof item !== "object" || item === null || !("message" in item) ||
+            typeof item.message !== "string"
+        ) {
+            throw new Error("Commit message is not a string.");
+        }
+    }
+}
