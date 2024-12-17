b630e590660f1a46abda7e0c3d7308bf1194aa95
Andreas Kjellqvist <andreas.kjellqvist@timeedit.com>
2024-12-17 14:29:25 +0100

    Remove unused index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const got = require("got");
-
-got.get("https://api.github.com/repos/doitadrian/contreebutors-action/pulls/2/commits", {
-    responseType: "json",
-}).then((response) => {
-    console.log(response.body);
-});
